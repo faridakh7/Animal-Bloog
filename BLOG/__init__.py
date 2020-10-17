0266d0ef189a6d22875a732ccfb1f7d751edf8a7
faridakh <farid.ahmadov97@gmail.com>
2020-10-17 21:59:28 +0400

    Login and Register

--- BLOG/__init__.py
+++ BLOG/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 from BLOG import routes
--- BLOG/routes.py
+++ BLOG/routes.py
@@ -1,6 +1,8 @@
 from flask  import render_template, flash, redirect, url_for
-from BLOG import app
+from BLOG import app, db
 from BLOG.forms import RegisterForm, LoginForm
+from BLOG.models import User, Post
+from flask_login import login_user, current_user, logout_user
 
 @app.route('/')
 @app.route('/home')
@@ -31,17 +33,35 @@ def contact():
 
 @app.route('/register',  methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = RegisterForm()
     if form.validate_on_submit():
-        flash(f'{form.username.data} account created', 'success')
-        return redirect(url_for('index'))
+        user = User(username=form.username.data, name=form.name.data, surname=form.surname.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'{form.username.data} adli hesab yaradildi', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='REGISTER', form=form)
 
 
 @app.route('/login',  methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'{form.username.data} account created', 'success')
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            return redirect(url_for('index'))
+
+        else:
+            flash(f'Düzgün daxil etməmisiniz', 'danger')
     return render_template('login.html', title='LOGIN', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
